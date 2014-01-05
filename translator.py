a57a54d12e7564e0d48e4d340ac85bdeb60a09e1
cjwinchester <cjwinchester@gmail.com>
2014-01-05 15:50:23 -0600

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,18 +1,35 @@
 import goslate
 from twitter import *
+from time import *
 
 gs = goslate.Goslate()
 
-OAUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXX'
-OAUTH_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
-CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXX'
-CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
+OAUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXX'
+OAUTH_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXX'
+CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXX'
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
-latest = t.statuses.user_timeline(screen_name="owhnews",count=1)
+# empty arrays
+owhtrans = []
+dowhtrans = []
 
-thing = latest[0]['text']
+# get last 100 owh tweets, push into empty array
+owhlatest = t.statuses.user_timeline(screen_name="owhnews",count=100)
 
-trans = gs.translate(thing, 'nl')
+for thing in owhlatest:
+    trans = gs.translate(thing['text'].encode('utf-8'), 'nl')   
+    owhtrans.append(trans)
 
-t.statuses.update(status=trans)
+# get last 100 dutchowh tweets, push into empty array
+dowhlatest = t.statuses.user_timeline(screen_name="dutchowh",count=100)
+
+for monkey in dowhlatest:
+    dowhtrans.append(monkey['text'])
+
+# find the differences and tweet 'em
+diffs = [x for x in owhtrans if x not in dowhtrans]
+
+for items in reversed(diffs):
+    t.statuses.update(status=items[:140])
+    sleep(10)
