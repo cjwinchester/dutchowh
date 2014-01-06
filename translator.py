d4a74e4884ec367f713cb3a95779e2d5f8622450
cjwinchester <cjwinchester@gmail.com>
2014-01-06 09:17:11 -0600

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,43 +1,48 @@
 import goslate
 from twitter import *
 from time import *
+from datetime import *
 
 gs = goslate.Goslate()
 
-OAUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXX'
-OAUTH_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
-CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXX'
-CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
+OAUTH_TOKEN = '2277709344-T6bZL6MTpCVcG48siSQoji6WAarkUazoluuqedM'
+OAUTH_SECRET = 'KGwvCFkUiwmIBOoDcj307MtLo8vWQnnTiyc0cjBHKXqDy'
+CONSUMER_KEY = 'Hi4B0ASWnbgGMTnJbGDiA'
+CONSUMER_SECRET = 'TlJEvdlqXrx4zmo94N1kYqdeTgVw1xGf4FAYnYHSwM'
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
-# create two empty lists
-owhtrans = []
-dowhtrans = []
+#create new list
+owhnew = []
 
-# get last 50 owh tweets, translate each and push them into empty list
-owhlatest = t.statuses.user_timeline(screen_name="owhnews",count=50)
+# get timestamp of last dutchOWH tweet
+dowhlatest = t.statuses.user_timeline(screen_name="dutchowh",count=1)
+thendowh = dowhlatest[0]['created_at']
+ds = datetime.strptime(thendowh, '%a %b %d %H:%M:%S +0000 %Y')
 
-for cowbell in owhlatest:
-    trans = gs.translate(cowbell['text'].encode('utf-8'), 'nl')
-    if len(trans) <= 140:
-        edittweet = trans.replace("@","").replace("  "," ").strip()
-        owhtrans.append(edittweet)
+# get last 100 owh tweets, translate each and tweet the ones posted after last dutchOWH tweet
+owhlatest = t.statuses.user_timeline(screen_name="owhnews",count=100)
+
+# loop through list
+for z in reversed(owhlatest):
+    
+    # translate, grab timestamp
+    trans = gs.translate(z['text'], 'nl')
+    ts = datetime.strptime(z['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+    
+    # email fix
+    edittweet = trans.replace(" @ owh.com","@owh.com").replace("  "," ").strip()
+
+    # skip tweets > 140 characters and older than last dutchOWH tweet
+    if len(edittweet) < 140 and ts > ds:
+        print edittweet
+        try:
+            owhnew.append(edittweet)
+            t.statuses.update(status=edittweet)
+        except TwitterHTTPError as e:
+            print "Something went wrong with this one. Skipping ..."
+            pass
+        sleep(10)
     else:
         pass
-
-# get last 50 dutchowh tweets, push into empty array
-dowhlatest = t.statuses.user_timeline(screen_name="dutchowh",count=50)
-for monkey in dowhlatest:
-    dowhtrans.append(monkey['text'].encode('utf-8'))
-
-# for find the differences, tweet 'em in reverse order
-diffs = list(set(owhtrans) - set(dowhtrans))
-
-for items in reversed(diffs):
-    if len(items) <= 140:
-        stat = items.replace("@","").replace("  "," ").strip()
-        print stat
-        t.statuses.update(status=stat)
-    else:
-        pass
-    sleep(10)
+        
+print "Tweeted " + str(len(owhnew)) + " things."
