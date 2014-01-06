b3266a9645579c0cc7e519ecd4d9f09f641eabb9
cjwinchester <cjwinchester@gmail.com>
2014-01-05 19:01:25 -0600

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,33 +4,40 @@ from time import *
 
 gs = goslate.Goslate()
 
-OAUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
-OAUTH_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
-CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
-CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
+OAUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXX'
+OAUTH_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
+CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
-# create empty arrays
+# create two empty lists
 owhtrans = []
 dowhtrans = []
 
-# get last 100 owh tweets, translate and push into an empty array
+# get last 50 owh tweets, translate each and push them into empty list
 owhlatest = t.statuses.user_timeline(screen_name="owhnews",count=50)
 
 for cowbell in owhlatest:
     trans = gs.translate(cowbell['text'].encode('utf-8'), 'nl')
-    owhtrans.append(trans)
+    if len(trans) <= 140:
+        edittweet = trans.replace("@","").replace("  "," ").strip()
+        owhtrans.append(edittweet)
+    else:
+        pass
 
-# get last 100 dutchowh tweets, push into empty array
+# get last 50 dutchowh tweets, push into empty array
 dowhlatest = t.statuses.user_timeline(screen_name="dutchowh",count=50)
 for monkey in dowhlatest:
     dowhtrans.append(monkey['text'].encode('utf-8'))
 
-# find the differences and tweet 'em
+# for find the differences, tweet 'em in reverse order
 diffs = list(set(owhtrans) - set(dowhtrans))
 
 for items in reversed(diffs):
-    stat = items[:140].replace("@","").replace("  "," ")
-    print stat
-    t.statuses.update(status=stat)
+    if len(items) <= 140:
+        stat = items.replace("@","").replace("  "," ").strip()
+        print stat
+        t.statuses.update(status=stat)
+    else:
+        pass
     sleep(10)
